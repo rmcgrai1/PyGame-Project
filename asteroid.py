e2296722385d455aa15ebfd929b277d47e07be1b
jkassman <jkassman@nd.edu>
2016-05-04 04:42:12 -0400

    Fixed Asteroid Scaling

--- asteroid.py
+++ asteroid.py
@@ -5,10 +5,11 @@ class Asteroid(object):
     MOD_ASTEROID = None;
     
 
-    def __init__(self, size, x, y, z, rotX, rotY, rotZ):
+    def __init__(self, a_id, size, x, y, z, rotX, rotY, rotZ):
         if Asteroid.MOD_ASTEROID == None:
             Asteroid.MOD_ASTEROID = canv3d.loadObj("asteroid.obj")
-        self.scale = size * 0.005;
+        self.scale = size * 0.0075;
+        self.a_id = a_id;
         self.x = x;
         self.y = y;
         self.z = z;
@@ -18,7 +19,6 @@ class Asteroid(object):
 
 
     def draw(self, screen):
-        #canv3d.setMatIdentity(MAT_T)
         canv3d.setMatTranslation(MAT_T, self.x, self.y, self.z)
         canv3d.addMatRotationX(MAT_T, self.rotX)
         canv3d.addMatRotationX(MAT_T, self.rotY)
