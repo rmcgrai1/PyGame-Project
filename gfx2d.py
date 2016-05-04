db999b45c975f0730c8f3766d73a6480729e97b2
rmcgrai1 <rmcgrai1@nd.edu>
2016-05-03 20:13:11 -0400

    Added colored fonts.

--- gfx2d.py
+++ gfx2d.py
@@ -28,8 +28,18 @@ from radar						import *
 import json
 
 
+FONT_WHITE = 0
+FONT_BLACK = 1
+FONT_RED = 2
+FONT_GREEN = 3
+FONT_BLUE = 4
+FONT_YELLOW = 5
+FONT_CYAN = 6
+FONT_MAGENTA = 7
+
 fontSprite = None
-fontCharList = []
+fontCharList = None
+subCharList = None
 fontWidth = 8
 fontHeight = 8
 
@@ -38,19 +48,53 @@ def init():
 	global fontCharList
 	
 	fontSprite = Sprite("img/font.png", 16,16)	
-	fontCharList = [None] * (16*16)
+	
+	# Create list of lists to store each letter in a different color
+	fontCharList = []
+	for i in range(0,8):
+		fontCharList.append( [None] * (16*16) )	
 	
 	for y in range(0,16):
 		for x in range(0,16):
-			c = y*16 + x			
-			fontCharList[c] = fontSprite.get(0,0, frame=c)[0].copy()
+			c = y*16 + x
+			
+			white = fontCharList[FONT_WHITE][c] = fontSprite.get(0,0, frame=c)[0].copy()			
+			
+			black = fontCharList[FONT_BLACK][c] = white.copy()
+			black.fill((0,0,0,255), special_flags=BLEND_RGB_MULT)
+
+			red = fontCharList[FONT_RED][c] = white.copy()
+			red.fill((255,0,0,255), special_flags=BLEND_RGB_MULT)
+
+			green = fontCharList[FONT_GREEN][c] = white.copy()
+			green.fill((0,255,0,255), special_flags=BLEND_RGB_MULT)
+			
+			blue = fontCharList[FONT_BLUE][c] = white.copy()
+			blue.fill((0,0,255,255), special_flags=BLEND_RGB_MULT)
+
+			yellow = fontCharList[FONT_YELLOW][c] = white.copy()
+			yellow.fill((255,255,0,255), special_flags=BLEND_RGB_MULT)
+			
+			magenta = fontCharList[FONT_MAGENTA][c] = white.copy()
+			magenta.fill((255,0,255,255), special_flags=BLEND_RGB_MULT)
+			
+			cyan = fontCharList[FONT_CYAN][c] = white.copy()
+			cyan.fill((0,255,255,255), special_flags=BLEND_RGB_MULT)
+			
+			
+	subCharList = fontCharList[FONT_WHITE]
 			
 def drawChar(surf, c, x,y):
-	surf.blit(fontCharList[ord(c)], (x,y, fontWidth,fontHeight))
+	surf.blit(subCharList[ord(c)], (x,y, fontWidth,fontHeight))
 	pass
+
 	
-def drawText(surf, txt, x,y):
+def drawText(surf, txt, x,y, color=FONT_WHITE):
+	global subCharList
+	subCharList = fontCharList[color]
+	
 	oriX = x
+	
 	for c in txt:	
 		if c == '\n':
 			x = oriX
@@ -60,3 +104,7 @@ def drawText(surf, txt, x,y):
 		else:
 			drawChar(surf, c, x,y)
 			x += fontWidth
+
+def drawTextShadow(surf, txt, x,y, color=FONT_WHITE):
+	drawText(surf, txt, x+2,y+1, FONT_BLACK);
+	drawText(surf, txt, x,y, color);
--- hud.py
+++ hud.py
@@ -65,25 +65,32 @@ class Hud(Drawable):
 	def blitToScreen(self, screen):
 		w = 96
 		h = 96
-			
+		xTB = 15+96+15
+		yTB = 480-15-96
+
+		# Draw Talk Bar
+		screen.blit(self.imgTalkBar, (xTB, yTB))
+
+		
 		if self.staticTime > 0:
 			h = int(h * (self.staticTimeMax-self.staticTime)/self.staticTimeMax)
 			self.imgStatic.draw(screen, 15+48, 480-48-15, frame=self.ind, scale=(.75, .75*(self.staticTimeMax-self.staticTime)/self.staticTimeMax))	
-		elif self.scaredTime > 0:
-			self.imgFlynn.draw(screen, 15+48, 480-48-15, frame=2, scale=.75)
-		else:
-			self.imgFlynn.draw(screen, 15+48, 480-48-15, frame=self.ind, scale=.75)
+		else:	
+			if self.scaredTime > 0:
+				self.imgFlynn.draw(screen, 15+48, 480-48-15, frame=2, scale=.75)
+			else:
+				self.imgFlynn.draw(screen, 15+48, 480-48-15, frame=self.ind, scale=.75)
+		
+			gfx2d.drawTextShadow(screen, "FLYNN", xTB+2,yTB+2, gfx2d.FONT_YELLOW)
+
+		# Draw Outline
 		pygame.draw.rect(screen, (255,255,255), (int(15+48-w/2),int(480-48-15-h/2), int(w),int(h)), 1)
+
 		
-		
-		xTB = 15+96+15
-		yTB = 480-15-96
-		screen.blit(self.imgTalkBar, (xTB, yTB))
-		
-		gfx2d.drawText(screen, self.text[:(int)(self.talkInd)], xTB+15,yTB+15)
+		gfx2d.drawTextShadow(screen, self.text[:(int)(self.talkInd)], xTB+15,yTB+15)
 	
 		if self.gs.isConnected:
-			gfx2d.drawText(screen, "Successfully connected!", 0,0)
+			gfx2d.drawTextShadow(screen, "Successfully connected!", 0,0)
 		else:
 			dotStr = "." * (3 - self.gs.connectTimer/self.gs.connectDiv % 4)
-			gfx2d.drawText(screen, "Attempting to connect" + dotStr, 0,0)
+			gfx2d.drawTextShadow(screen, "Attempting to connect" + dotStr, 0,0)
